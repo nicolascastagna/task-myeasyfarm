ad66340c8446a2d1831f9175168fa2d1a8f4642b
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2022-11-25 11:44:11 +0100

    adding new task + geojson

---
+++ 0-README.txt
@@ -0,0 +1,28 @@
+Input:
+- 2 files with responses from the server (a subset of fields and a subset of soil maps);
+- logo and colors guidance;
+- UX sketch;
+- reference screenshots from the real app, showing some of the maps and fields.
+
+You need to make a small web app with the following requirements:
+- An app should have a welcome screen with a start button at the very center of the page;
+- After clicking on the start you should be presented with the map, where fields (from partfield.json) are displayed;
+- You should be able to click on the field and see a list of available maps for this field;
+- You should be able to click on a map to preview it;
+- During preview app should focus (zoom/center) on the map you are previewing;
+- You should be able to exit the preview screen by clicking on the close button;
+- Maps should either use predefined colors or be colorized based on the value;
+- On the top left corner the logo should always be visible.
+
+
+Additional info
+- You need to use React and typescript;
+- You can use any project template you want as a starting point;
+- partfield == field (different terms for the same object);
+- You should not spend on this task more than 4h;
+- The use of search engines is not only not prohibited but also encouraged.
+
+Evaluation criteria
+- App works;
+- Good code, following best practices;
+- Attention to detail.
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import soilmapData from "../data/soilmap.json";
-import partfieldsData from "../data/partfield.json";
+import React, { useEffect, useState } from "react";
+import soilmapData from "../data/soilmaps.json";
+import partfieldsData from "../data/partfields.json";
 import {
   MapContainer,
   TileLayer,
@@ -11,18 +11,28 @@ import {
 
 const Map = () => {
   const [isPreview, setIsPreview] = useState(false);
+  const [coordinatesCenter, setCoordinatesCenter] = useState([]);
+
   const MapCenter = [
     partfieldsData.items[0].center[1],
     partfieldsData.items[0].center[0],
   ];
 
-  const handlePreview = () => {
-    console.log(isPreview);
+  useEffect(() => {
+    setCoordinatesCenter(MapCenter);
+  }, []);
+
+  const handlePreview = (e) => {
+    soilmapData.items.filter((el) => el.partfield_id === soilmapData.items.id);
     setIsPreview(true);
   };
 
-  // console.log(partfieldsData.items[0].center[1]);
-  console.log(soilmapData.data.features);
+  const closePreview = () => {
+    setIsPreview(false);
+  };
+
+  console.log(soilmapData);
+
   return (
     <div>
       <MapContainer center={MapCenter} zoom={14} scrollWheelZoom={true}>
@@ -36,67 +46,72 @@ const Map = () => {
             item[1],
             item[0],
           ]);
+
           const colorFields = `#${state.color_hex}`;
           return (
             <Polygon
               key={i}
-              pathOptions={{ colorFields }}
+              fillColor={colorFields}
+              color={colorFields}
+              weight={2}
               positions={coordinates}
+              onClick={() => handlePreview(state.id, state.coords[0])}
             >
               <Popup>
                 <p className="title-preview">Available maps:</p>
-                {partfieldsData.id === soilmapData.data.partfield_id ||
-                  (soilmapData.data.id &&
-                    soilmapData.data.features.map((i) => {
-                      return i.features.map((feature, index) => {
-                        return (
-                          <>
-                            <span className="contain-preview">
-                              {!isPreview ? (
-                                <span
-                                  key={index}
-                                  className="btn-preview"
-                                  onClick={() => handlePreview()}
-                                >
-                                  {feature.type} n° {index + 1} - Preview
-                                </span>
-                              ) : (
-                                <div className="display-preview">
-                                  <h4
-                                    className="title-cancel"
-                                    // onClick={setIsPreview(false)}
-                                  >
-                                    Cancel Preview
-                                  </h4>
-                                  <MapContainer
-                                    center={MapCenter}
-                                    zoom={14}
-                                    scrollWheelZoom={true}
-                                  >
-                                    <TileLayer
-                                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                    />
-                                    <Polygon
-                                      paths={isPreview}
-                                      pathOptions={{
-                                        color: feature.color,
-                                      }}
-                                      positions={feature.geometry.coordinates}
-                                    />
-                                  </MapContainer>
-                                </div>
-                              )}
-                            </span>
-                          </>
-                        );
-                      });
-                    }))}
+                <p className="subtitle">Type:</p>
+                <>
+                  <ul className="soil-name">
+                    {state.soil ? (
+                      <>
+                        <li>{state.soil}</li>
+                        <span
+                          className="link-soil"
+                          onClick={() => {
+                            handlePreview();
+                          }}
+                        >
+                          Preview
+                        </span>
+                      </>
+                    ) : (
+                      "No map available"
+                    )}
+                  </ul>
+                </>
               </Popup>
+              {isPreview &&
+                partfieldsData.items.id === soilmapData.items.partfield_id &&
+                soilmapData.items.map(function (item, index) {
+                  //   const polygon =
+                  //   item.features.geometry.coordinates[0][0].map(
+                  //     (item) => [item[1], item[0]]
+                  //   );
+                  // console.log(polygon);
+                  // const color = `#${map.color_hex}`;
+
+                  return (
+                    <GeoJSON data={item.mapdata.features} key={index.id} />
+                  );
+                  // return (
+                  //   <Polygon
+                  //     key={"soil-" + index + "-" + item.id}
+                  //     fillColor={colorFields}
+                  //     color={colorFields}
+                  //     weight={2}
+                  //     positions={item.mapdata.features[0].geometry.coordinates}
+                  //   />
+                  // );
+                })}
             </Polygon>
           );
         })}
       </MapContainer>
+      {isPreview ? (
+        <button className="cancel-preview" onClick={closePreview}>
+          Cancel Preview
+        </button>
+      ) : null}
     </div>
   );
 };
